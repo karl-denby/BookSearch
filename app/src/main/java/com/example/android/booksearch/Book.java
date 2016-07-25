9698a0c4a6bb5172b9b13f1d81fc57ba30ff2ac0
karl-denby <karl.denby@gmail.com>
2016-07-25 19:53:48 +0100

    implements Book class

---
+++ app/src/main/java/com/example/android/booksearch/Book.java
@@ -0,0 +1,20 @@
+package com.example.android.booksearch;
+
+/**
+ * Created by karld on 25/07/2016.
+ * Simple Book class we will use for prototype
+ * Later we will flesh this out and modify as needed.
+ */
+public class Book {
+    private String mTitle;
+    private String mAuthor;
+
+    public Book(String title, String author) {
+        title = mTitle;
+        author = mAuthor;
+    }
+
+    public String getTitle() {return mTitle;}
+
+    public String getAuthor() {return mAuthor;}
+}
